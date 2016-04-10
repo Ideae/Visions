d589978006bba9f7a17532935a7a2dbb6f90ce68
Zack Harris <zack.tibia@gmail.com>
2016-04-10 07:05:11 -0400

    pointlessness

--- A3/code_and_images/checkOverlap.m
+++ A3/code_and_images/checkOverlap.m
@@ -7,14 +7,14 @@ function isOverlapping = checkOverlap( bbox1, bbox2, threshold)
     isOverlapping = false;
     if iw>0 && ih>0       
         % compute overlap as area of intersection / area of union
-        ua=(bbox1(3)-bbox1(1)+1)*(bbox1(4)-bbox1(2)+1)+...
-           (bbox2(3)-bbox2(1)+1)*(bbox2(4)-bbox2(2)+1)-...
-           iw*ih;
-%         w1=bbox1(3)-bbox1(1)+1;
-%         h1=bbox1(4)-bbox1(2)+1;
-%         w2=bbox2(3)-bbox2(1)+1;
-%         h2=bbox2(4)-bbox2(2)+1;
-%         ua = min(w1*h1,w2*h2);
+%         ua=(bbox1(3)-bbox1(1)+1)*(bbox1(4)-bbox1(2)+1)+...
+%            (bbox2(3)-bbox2(1)+1)*(bbox2(4)-bbox2(2)+1)-...
+%            iw*ih;
+        w1=bbox1(3)-bbox1(1)+1;
+        h1=bbox1(4)-bbox1(2)+1;
+        w2=bbox2(3)-bbox2(1)+1;
+        h2=bbox2(4)-bbox2(2)+1;
+        ua = min(w1*h1,w2*h2);
         ov=iw*ih/ua;
         isOverlapping = ov>threshold;
     end
